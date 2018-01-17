c197de7436c55cf2016cbc457eead5f92e0292b8
JealJelly <JealJelly@github.com>
2018-01-17 14:59:45 +0300

    Fixed hound's BARKs

--- tasks/SK/first_calc.rb
+++ tasks/SK/first_calc.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
 def request_input
   puts 'Введите пример (для выхода введите exit):'
   request = gets
   request = request.chomp
-  if request == 'exit'
-    return 0
-  end
-  if request.scan(/^(?:\d+(\.\d+)?\s*[*+\/-])+\s*\d+(\.\d+)?$/) == []
+  return 0 if request == 'exit'
+  if request.scan(%r(/^(?:\d+(\.\d+)?\s*[*+\/-])+\s*\d+(\.\d+)?$/)) == []
     puts 'Некорректный ввод'
   else
     request_parse(request)
@@ -14,14 +13,12 @@ def request_input
 end
 
 def request_parse(request)
-  operator = request.scan(/[*+\/-]/)[0]
+  operator = request.scan(%r(/[*+\/-]/))[0]
   first_value = request.split(operator)[0].to_f
   second_value = request.split(operator)[1].to_f
   result = eval ("#{first_value}#{operator}#{second_value}")
-  if result%1==0
-    result = result.to_i
-  end
-  puts request+"=#{result}"
+  result = result.to_i if (result % 1).zero?
+  puts request + "=#{result}"
 end
 
 def run_calculator
